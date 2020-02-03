f6028843d2a205a7853123185901dab3cb2666f7
parvej <parvej1114@yahoo.com>
2020-02-03 21:01:45 +0800

    It's the initial commit and it's the testing commit

---
+++ src/org/launchcode/Teacher.java
@@ -0,0 +1,14 @@
+package org.launchcode;
+
+/* It,s a simple class to understand how class and object work */
+public class Teacher {
+    String name;
+    String gender;
+    int age;
+
+    void display(){
+        System.out.println("Name: " + name);
+        System.out.println("Gender: " + gender);
+        System.out.println("Age: " + age);
+    }
+}
---
+++ src/org/launchcode/TestTeacher.java
@@ -0,0 +1,11 @@
+package org.launchcode;
+
+public class TestTeacher {
+    public static void main(String[] args) {
+        Teacher t1 = new Teacher();
+        t1.name = "Parvej";
+        t1.gender = "Male";
+        t1.age = 26;
+        t1.display();
+    }
+}
