59f2951f3f94942dd2d4a0d6f9f9f4cda558e3c7
parvej <parvej1114@yahoo.com>
2020-02-08 11:06:37 +0800

    returning value from function

---
+++ src/returning_value/Square.java
@@ -0,0 +1,7 @@
+package returning_value;
+
+public class Square {
+    int square(int num1){
+        return num1*num1;
+    }
+}
---
+++ src/returning_value/TestSquare.java
@@ -0,0 +1,8 @@
+package returning_value;
+
+public class TestSquare {
+    public static void main(String[] args) {
+        Square s1 = new Square();
+        System.out.println("Square of 5 is: " + s1.square(5));
+    }
+}
