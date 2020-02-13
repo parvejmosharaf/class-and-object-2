3d8507f8f6741b1d0945d2c53f2c102dd41b2c4b
parvej <parvej1114@yahoo.com>
2020-02-13 18:33:56 +0800

    ClassName.static_variable

---
+++ src/static_keyword/StaticVariable.java
@@ -0,0 +1,7 @@
+package static_keyword;
+
+public class StaticVariable {
+    String name;
+    int id;
+    static String address = "Hangzhou Dianzi U";
+}
---
+++ src/static_keyword/TestStaticVariable.java
@@ -0,0 +1,8 @@
+package static_keyword;
+
+public class TestStaticVariable {
+    public static void main(String[] args) {
+        StaticVariable ob1 = new StaticVariable();
+        System.out.println("University: " + StaticVariable.address);
+    }
+}
